92dfcb61b48eab022104b39e610d52a077cf1763
Sahil Gupta <55432865+error-hacker@users.noreply.github.com>
2020-03-31 20:12:25 +0530

    Add files via upload

--- LineEncoderAndScrambler.py
+++ LineEncoderAndScrambler.py
@@ -19,7 +19,7 @@ def NRZL(dataStream):
                 d.append(amp)
         elif int(dataStream[i])==0:
             for j in range(0,100):
-                d.append(amp)
+                d.append(-amp)
     plt.plot(d)
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
@@ -122,130 +122,12 @@ def AMI(dataStream):
     plt.show()
     return
 
-def B8ZS(dataStream):
-    d=[]
-    amp=-4
-    Y=[i for i in range(amp-1,-amp+2)]
-    a=1
-    for i in range(0,len(dataStream)):
-        X=[100*i for y in range(0,len(Y))]
-        plt.plot(X,Y,'--', color='grey')
-        if int(dataStream[i])==0:
-            c=0
-            if a==1:
-                for k in range(1,8):
-                    if i+k>=len(dataStream):
-                        c=1
-                        break
-                    elif int(dataStream[i+k]) == 0:
-                        continue
-                    else:
-                        c=1
-                        break
-                if c==0:
-                    x=0
-                    for j in range(0,300):
-                        d.append(0)
-                    for j in range(0,100):
-                        d.append(amp)
-                    for j in range(0,100):
-                        d.append(-amp)
-                    for j in range(0,100):
-                        d.append(0)
-                    for j in range(0,100):
-                        d.append(-amp)
-                    for j in range(0,100):
-                        d.append(amp)
-                else:
-                    x=1
-                    for j in range(0,100):
-                        d.append(0)
-                    
-            elif a==8:
-                a=1
-                x=1
-                
-            if x==0:
-                a+=1
-                
-        elif int(dataStream[i])==1:
-            amp=-amp
-            for j in range(0,100):
-                d.append(amp)
-    plt.plot(d)
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.title('B8ZS Scrambling')
-    plt.show()
-    return
 
-def HDB3(dataStream):
-    d=[]
-    amp=-4
-    Y=[i for i in range(amp-1,-amp+2)]
-    a=1
-    count=0
-    for i in range(0,len(dataStream)):
-        X=[100*i for y in range(0,len(Y))]
-        plt.plot(X,Y,'--', color='grey')
-        if int(dataStream[i])==0:
-            c=0
-            if a==1:
-                for k in range(1,4):
-                    if i+k>=len(dataStream):
-                        c=1
-                        break
-                    elif int(dataStream[i+k]) == 0:
-                        continue
-                    else:
-                        c=1
-                        break
-                if c==0:
-                    x=0
-                    if count%2 == 0:
-                        amp=-amp
-                        for j in range(0,100):
-                            d.append(amp)
-                        count+=1
-                        for j in range(0,200):
-                            d.append(0)
-                        for j in range(0,100):
-                            d.append(amp)
-                        count+=1
-                    elif count%2 != 0:
-                        for j in range(0,300):
-                            d.append(0)
-                        for j in range(0,100):
-                            d.append(amp)
-                        count+=1
-                else:
-                    x=1
-                    for j in range(0,100):
-                        d.append(0)
-                    
-            elif a==4:
-                a=1
-                x=1
-                
-            if x==0:
-                a+=1
-                
-        elif int(dataStream[i])==1:
-            amp=-amp
-            for j in range(0,100):
-                d.append(amp)
-            count+=1
-    plt.plot(d)
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.title('HDB3 Scrambling')
-    plt.show()
-    return
 
 while 1:
-    print('Please enter the encoding you want to perform: \n1 - NRZL \n2 - NRZI \n3 - Manchester \n4 - Differential Manchester \n5 - Bipolar-AMI \n6 - B8ZS \n7 - HDB3 \n0 - Quit')
+    print('Please enter the encoding you want to perform: \n1 - NRZL \n2 - NRZI \n3 - Manchester \n4 - Differential Manchester \n5 - Bipolar-AMI \n0 - Quit')
     choice=' '
-    while choice not in '1 2 3 4 5 6 7 0'.split():
+    while choice not in '1 2 3 4 5 0'.split():
         choice = input ('Enter your choice : ')
     if choice == '0':
         break
@@ -266,9 +148,4 @@ while 1:
     if choice=='5':
         AMI(dS)
         break
-    if choice=='6':
-        B8ZS(dS)
-        break
-    if choice=='7':
-        HDB3(dS)
-        break
+    
